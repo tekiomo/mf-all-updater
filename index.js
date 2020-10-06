ad8c0c7e96c78d68b52b97c3368fd508f891e4c2
ABE Tomonori <tekiomo@gmail.com>
2020-10-06 18:04:47 +0900

    Add ua

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const main = async () => {
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
+  page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36');
 
   try {
     // id.moneyforward.com で直接ログインすると、 moneyforward.com にはログインできない
